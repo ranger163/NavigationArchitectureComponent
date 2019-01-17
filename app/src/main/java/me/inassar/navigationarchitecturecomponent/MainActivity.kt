aecdf9903215796844794a49655a74ffafbbd537
Nassar163 <ahmed.nassar163@gmail.com>
2019-01-17 17:12:40 +0200

    Video 01_03

--- app/src/main/java/me/inassar/navigationarchitecturecomponent/MainActivity.kt
+++ app/src/main/java/me/inassar/navigationarchitecturecomponent/MainActivity.kt
@@ -3,16 +3,24 @@ package me.inassar.navigationarchitecturecomponent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
+import me.inassar.navigationarchitecturecomponent.dummy.DummyContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ListFragment.OnListFragmentInteractionListener {
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+        Log.i("Navigation", "Selected $item")
+    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
+                findNavController(R.id.nav_host).navigate(R.id.main_dest)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
+                findNavController(R.id.nav_host).navigate(R.id.list_dest)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
